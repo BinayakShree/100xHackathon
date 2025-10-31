2bfa52e8a5ffbbd007185add071d7e89dd0ed9ba
BinayakShree <bshreeshrestha@gmail.com>
2025-11-01 02:03:23 +0545

    BROO

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
-import express, { type Application } from "express";
+import express from "express";
+import type { Application } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
@@ -6,12 +7,16 @@ import authRoutes from "./routes/authRoutes";
 dotenv.config();
 
 const app: Application = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Express + Prisma + NeonDB API is running");
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
