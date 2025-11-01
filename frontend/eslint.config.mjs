e4f4ca31d18c31f50bba657a976b2ea0fa40cd3e
ShreyasBhatarai <ShreyasBhattarai@gmail.com>
2025-11-01 16:32:39 +0545

    yubduasdi

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,7 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -20,6 +19,35 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      // Disable all rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "no-undef": "off",
+      "no-empty": "off",
+      // Disable all other rules
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off"
+    },
+  }
 ];
 
 export default eslintConfig;
